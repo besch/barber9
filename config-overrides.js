6fae8d4199d88584ecec9db35342a13bddfc96c2
besch <vinogradskiy.n@gmail.com>
2022-08-08 10:41:37 +0200

    build to docs

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
-const path = require('path');
-module.exports = {
-    paths: function (paths, env) {
-        paths.appPublic = path.resolve(__dirname, 'docs');
-        paths.appHtml = path.resolve(__dirname, 'docs/index.html');
-        return paths;
-    }
-}
+// const path = require('path');
+// module.exports = {
+//     paths: function (paths, env) {
+//         paths.appPublic = path.resolve(__dirname, 'docs');
+//         paths.appHtml = path.resolve(__dirname, 'docs/index.html');
+//         return paths;
+//     }
+// }

99366ba9a8a28afb3097f70bcc01b0db7e6f2355
besch <vinogradskiy.n@gmail.com>
2022-08-08 10:10:11 +0200

    react rewired

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    paths: function (paths, env) {
+        paths.appPublic = path.resolve(__dirname, 'docs');
+        paths.appHtml = path.resolve(__dirname, 'docs/index.html');
+        return paths;
+    }
+}
